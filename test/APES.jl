51be16ff79aa827a58a61c6a11d282c722d52325
Robert Luke <robert.g.luke@gmail.com>
2014-11-27 11:53:42 +0100

    Updated tests

--- test/APES.jl
+++ test/APES.jl
@@ -1,4 +1,6 @@
-# Test signal properties
+#
+# Test signal
+#
 
 ## Li & Stoica fig1
 spectral_lines_f = [0.04, 0.05, 0.08, 0.09,    0.16:0.005:0.35]
@@ -11,15 +13,72 @@ noise_mean = 0
 noise_vari = 1
 
 ## Create example signal
-z = vec(zeros(N, 1))
-n = 1.0:N
-for i in 1:length(spectral_lines_f)
-    z = z + spectral_lines_a[i] * sin(2 * pi * spectral_lines_f[i] * n + spectral_lines_p[i])
+function create_example(N, f, a, p)
+    z = vec(zeros(N, 1))
+    n = 1.0:N
+    for i in 1:length(f)
+        z = z + a[i] * sin(2 * pi * f[i] * n + p[i])
+    end
+    return z
 end
-srand(1) # For reproducible results
-z += sqrt(noise_vari)*randn(size(z))
 
-# Run algorithm
-results = apes(z, spectral_lines_f)
+# For passing in known cov matrix
+function covariance_matrix{T <: FloatingPoint}(s::Array{T}; M::Int=int(length(s)/2))
+    N = length(s)
+    L = N - M + 1
+    CovMatrix = zeros(M, M)
+    for l = 0:N-M
+        CovMatrix += s[l+1 : 1 : l+M-1+1] * s[l+1 : 1 : l+M-1+1]'
+    end
+    CovMatrix ./ L
+end
 
-@test abs(results[1:4]) == [10.150160442095103,9.871766839666588,10.024069735757875,2.1317568708681485]
+# For reproducible results
+srand(1)
+
+
+#
+# Test example from Li & Stoica
+#
+
+signal = create_example(N, spectral_lines_f, spectral_lines_a, spectral_lines_p)
+noise  = sqrt(noise_vari) * randn(size(signal))
+sn     = signal + noise
+MSE    = mean((abs(apes(sn, spectral_lines_f)) .- spectral_lines_a).^2)
+
+@test_approx_eq abs(apes(sn, spectral_lines_f)[1:4])  [10.150160442095107,9.871766839666593,10.02406973575788,2.131756870868149]
+@test_approx_eq MSE  0.10794065421715185
+
+
+#
+# Print pretty figures
+#
+
+#=using Gadfly=#
+#=using DSP=#
+#=f = 0.0:0.001:0.5=#
+#=a = abs(apes(sn, f))=#
+#=a = layer(x=f, y=a, Geom.line,Theme(default_color=color("green")))=#
+#=f = layer(x=freq(periodogram(sn, nfft=N*16)), y=sqrt(power(periodogram(sn, nfft=N*16))/(length(sn)/2)), Geom.line,Theme(default_color=color("red")))=#
+#=p = plot(a, f, Guide.title("FFT (red) APES(green)"), Guide.xlabel("Frequency"), Guide.ylabel("Amplitude"))=#
+#=draw(PNG("Eg.png", 28cm, 14cm), p)=#
+
+
+#
+# Test at lower SNR
+#
+
+signal  = create_example(N, spectral_lines_f, spectral_lines_a, spectral_lines_p)
+noise   = sqrt(noise_vari) * randn(size(signal))
+sn      = signal + noise * 50
+MSE     = mean((abs(apes(sn, spectral_lines_f)) .- spectral_lines_a).^2)
+MSE_w_Q = mean((abs(apes(sn, spectral_lines_f, Q = covariance_matrix(noise))) .- spectral_lines_a).^2)
+
+@test_approx_eq MSE     92.84644647200092
+@test_approx_eq MSE_w_Q 9.936179958764319
+
+
+
+
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,18 @@
 using APES
 using Base.Test
 using Lint
+using TypeCheck
 
+# Run tests
 include("APES.jl")
+include("APES-speed.jl")
 
+# Check code
 lintpkg( "APES" )
+
+checkreturntypes(APES)
+println()
+checklooptypes(APES)
+println()
+checkmethodcalls(APES)
+
